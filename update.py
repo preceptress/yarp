2b4818e5d51f83813bc9bfa5d566facad149bb0a
chief <edwardpotter@gmail.com>
2022-08-02 00:50:58 +0000

    working

--- update.py
+++ update.py
@@ -1,7 +1,14 @@
 # updates.py
+# import requests
+
+import os
 import praw
 import psycopg2
 
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 def error_response(status_code, message=None):
     payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
@@ -14,24 +21,19 @@ def error_response(status_code, message=None):
 def bad_request(message):
     return error_response(400, message)
 
-reddit = praw.Reddit(client_id='abcdefghijklmnopqrstuvwxyz',
-                     client_secret='abcdefghijklmnopqrstuvwxyz',
-                     user_agent='my user agent',
-                     thing_limit=100)
+# connect with praw pass credationals
+reddit = praw.Reddit(client_id=os.getenv('client_id'),client_secret=os.getenv('client_secret'),user_agent=os.getenv('user_agent'),thing_limit=os.getenv('thing_limit'))
 
 # try to connect
-try:
-    connection = psycopg2.connect(
-        "dbname='mydb' user='myuser' password='abcdefghijklmnopqrstuvwxyz'")
-except:
-    print("Error: Unable to connect to database.")
+conn_string = "dbname=%s user=%s password=%s" % (os.getenv('db_name'),os.getenv('db_user'), os.getenv('db_password'))
+connection = psycopg2.connect(conn_string)
 
-# success
+# sucess
 cursor = connection.cursor()
 
 def update():
 
-    # loop through our Subreddit choices
+    # loop through our choices
     choicesArray = ["reddit",
                     "funny",
                     "AskReddit",
@@ -45,7 +47,6 @@ def update():
     for j in choicesArray:
 
         for submission in reddit.subreddit(j).new(limit=3):
-
             my_title = submission.title
             my_permalink = "https://www.reddit.com" + submission.permalink
             my_url = submission.url
@@ -55,17 +56,16 @@ def update():
 
             try:
                 cursor.execute(
-                    '''INSERT INTO submissions(title,subreddit,permalink,url,utc,comments,score) VALUES(%s,%s,%s,%s,%s,%s,%s) ON CONFLICT(permalink) DO NOTHING''', (my_title, j, my_permalink, my_url, my_utc, my_num_comments, my_score))
+                    '''INSERT INTO submissions(title,subreddit,permalink,url,utc,comments,score) VALUES(%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING''', (my_title, j, my_permalink, my_url, my_utc, my_num_comments, my_score))
                 connection.commit()
 
             except (Exception, psycopg2.Error) as error:
                 if(connection):
-                    print("Failed to insert record into table", error)
+                    print("Failed to insert record into mobile table", error)
             finally:
-                print("Success. Record Inserted or Ignored if duplicate")
+                print("Sucess! Record was added  or ignored if duplicate")
 
             print(j)
-
 
 # INIT update
 update()
